b19961d8596b10ed11503679512317c7aa8f407c
Daphne-Herve <daphne.herve@gmail.com>
2022-02-18 07:20:45 +0100

    update matchmedia method in script.js

--- script.js
+++ script.js
@@ -15,10 +15,30 @@ function toggleMenu() {
   }
 }
 
-if (window.matchMedia("(min-width: 500px)").matches) {
+if (window.matchMedia("(max-width: 500px)").matches) {
+let iconOpen = '<img style="width:60%; margin-top:0;" src="./icon-open.png" alt=""/>' ;
+let iconClose = '<img style="width:60%; margin-top:0;" src="./icon-close.png" alt=""/>';
+
+function openMenu() {
+  this.icon.innerHTML = iconOpen;
+  this.panel.style.width="0%";
+  this.burgerMenu.style.marginLeft="85%";
+  this.burgerMenu.style.transition="0.5s";
+  this.panel.style.transition="0.5s";
+}
+
+function closeMenu() {
+  this.icon.innerHTML = iconClose;
+  this.panel.style.width="100%";
+  this.burgerMenu.style.marginLeft="5%";
+  this.burgerMenu.style.transition="0.3s";
+  this.panel.style.transition="0.5s";
+  this.menu = false;
+}
+
+} else {
   let iconOpen = '<img style="width:80%;" src="./icon-open.png" alt=""/>' ;
   let iconClose = '<img style="width:80%;" src="./icon-close.png" alt=""/>';
-  /* La largeur minimum de l'affichage est 481 px inclus */
   function openMenu() {
     this.icon.innerHTML = iconOpen;
     this.panel.style.width="0%";
@@ -37,27 +57,6 @@ if (window.matchMedia("(min-width: 500px)").matches) {
     this.panel.style.transition="0.5s";
     this.overlay.style.transition="1.5s";
     this.overlay.style.opacity="1";
-    this.menu = false;
-  }
-} else {
-  /* L'affichage est inférieur à 481 px de large */
-  let iconOpen = '<img style="width:60%; margin-top:0;" src="./icon-open.png" alt=""/>' ;
-  let iconClose = '<img style="width:60%; margin-top:0;" src="./icon-close.png" alt=""/>';
-
-  function openMenu() {
-    this.icon.innerHTML = iconOpen;
-    this.panel.style.width="0%";
-    this.burgerMenu.style.marginLeft="85%";
-    this.burgerMenu.style.transition="0.5s";
-    this.panel.style.transition="0.5s";
-  }
-
-  function closeMenu() {
-    this.icon.innerHTML = iconClose;
-    this.panel.style.width="100%";
-    this.burgerMenu.style.marginLeft="5%";
-    this.burgerMenu.style.transition="0.3s";
-    this.panel.style.transition="0.5s";
     this.menu = false;
   }
 }
