54f2da4fce4d643cd463cd642263c65baae2fc70
Daphne-Herve <daphne.herve@gmail.com>
2022-02-20 05:27:47 +0100

    update index.html

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ function toggleMenu() {
 
 // if (window.matchMedia("(max-width: 740px)").matches) {
 
-let iconOpen = '<img style="width:70%; margin-top:0;" src="./icon-open.png" alt=""/>' ;
-let iconClose = '<img style="width:70%; margin-top:0;" src="./icon-close.png" alt=""/>';
+let iconOpen = '<img style="width:70%; margin-top:0;" src="../assets/icon-open.png" alt=""/>' ;
+let iconClose = '<img style="width:70%; margin-top:0;" src="../assets/icon-close.png" alt=""/>';
 
   function openMenu() {
     this.icon.innerHTML = iconOpen;
@@ -35,7 +35,7 @@ let iconClose = '<img style="width:70%; margin-top:0;" src="./icon-close.png" al
     this.icon.innerHTML = iconClose;
     this.panel.style.width="100%";
     this.burgerMenu.style.marginLeft="5%";
-    this.burgerMenu.style.transition="0.3s";
+    this.burgerMenu.style.transition="0.5s";
     this.panel.style.transition="0.5s";
     this.menu = false;
   }
