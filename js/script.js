a0d71f5d19f6c7086297580b17e9208cf2d1d2d8
Daphne-Herve <daphne.herve@gmail.com>
2022-02-20 05:35:26 +0100

    change icon path in script.js

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ function toggleMenu() {
 
 // if (window.matchMedia("(max-width: 740px)").matches) {
 
-let iconOpen = '<img style="width:70%; margin-top:0;" src="./assets/icon-open.png" alt=""/>' ;
-let iconClose = '<img style="width:70%; margin-top:0;" src="./assets/icon-close.png" alt=""/>';
+let iconOpen = '<img style="width:70%; margin-top:0;" src="../assets/icon-open.png" alt=""/>' ;
+let iconClose = '<img style="width:70%; margin-top:0;" src="../assets/icon-close.png" alt=""/>';
 
   function openMenu() {
     this.icon.innerHTML = iconOpen;
@@ -39,34 +39,3 @@ let iconClose = '<img style="width:70%; margin-top:0;" src="./assets/icon-close.
     this.panel.style.transition="0.5s";
     this.menu = false;
   }
-
-// } else {
-//   let iconOpen = '<img style="width:80%;" src="./icon-open.png" alt=""/>' ;
-//   let iconClose = '<img style="width:80%;" src="./icon-close.png" alt=""/>';
-//   function openMenu() {
-//     this.icon.innerHTML = iconOpen;
-//     this.panel.style.width="0%";
-//     this.burgerMenu.style.marginLeft="95%";
-//     this.burgerMenu.style.transition="0.5s";
-//     this.panel.style.transition="0.5s";
-//     this.overlay.style.transition="1.5s";
-//   }
-
-//   function closeMenu() {
-//     this.icon.innerHTML = iconClose;
-//     this.panel.style.width="40%";
-//     this.burgerMenu.style.marginLeft="58%";
-//     this.burgerMenu.style.transition="0.3s";
-//     this.panel.style.transition="0.5s";
-//     this.menu = false;
-//   }
-// }
-
-
-// if (window.matchMedia("(min-width: 600px)").matches) {
-//   console.log('je suis grand')
-// } else {
-//   /* L'affichage est inférieur à 600px de large */
-//   console.log('je suis petit')
-
-// }
